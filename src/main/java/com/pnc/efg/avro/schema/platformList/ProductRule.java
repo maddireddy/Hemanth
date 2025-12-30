e3225676da53fe14ed9936f6a0977db1d0f9199a
Siva Maddireddy <maddireddynu@gmail.com>
2025-12-30 15:51:06 -0500

    Preod

--- src/main/java/com/pnc/efg/avro/schema/platformList/ProductRule.java
+++ src/main/java/com/pnc/efg/avro/schema/platformList/ProductRule.java
@@ -5,53 +5,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+
 public class ProductRule implements Serializable {
+    
     private String product;
-    private List<String> rules;
-
-    public ProductRule() {
-        this.rules = new ArrayList<>();
-    }
-
-    public ProductRule(String product, List<String> rules) {
-        this.product = product;
-        this.rules = rules != null ? new ArrayList<>(rules) : new ArrayList<>();
-    }
-
+    private String rules;
     public String getProduct() {
         return product;
     }
-
     public void setProduct(String product) {
         this.product = product;
     }
-
-    public List<String> getRules() {
+    public String getRules() {
         return rules;
     }
-
-    public void setRules(List<String> rules) {
-        this.rules = rules != null ? new ArrayList<>(rules) : new ArrayList<>();
+    public void setRules(String rules) {
+        this.rules = rules;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ProductRule that = (ProductRule) o;
-        return Objects.equals(product, that.product) && Objects.equals(rules, that.rules);
-    }
+    
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(product, rules);
-    }
-
-    @Override
-    public String toString() {
-        return "ProductRule{" +
-                "product='" + product + '\'' +
-                ", rules=" + rules +
-                '}';
-    }
 }
